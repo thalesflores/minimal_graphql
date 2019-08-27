ae7d928e3a0d6c0c9c61a6627cb594313914e597
Thales Flores <thalesvflores@gmail.com>
2019-08-26 23:20:34 -0300

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MinimalGraphql.MixProject do
     [
       app: :minimal_graphql,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
